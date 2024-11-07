e346ad5ec17d060e7cfc984225130d80f4d1bd30
Santiago-Jared <santiagojaredavalos@gmail.com>
2024-11-07 13:38:52 -0600

    Ejercicios4.py

---
+++ Ejercicios4.py
@@ -0,0 +1,17 @@
+#4.- Dados dos números, mostrar la suma, resta, división y multiplicación de ambos.
+
+
+num1 = input("Ingresa el primer numero para realizar LAS OPERACIONES")
+num1 = int (num1)
+num2 = input("Ingresa el segundo numero para realizar LAS OPERACIONES")
+num2 = int (num2)
+
+suma = int (num1 + num2)
+resta = int (num1-num2)
+multiplicacion = int (num1*num2)
+divisioin = int (num1/num2)
+
+print("EL RESULTADO DE LA SUMA ES: ", suma)
+print("EL RESULTADO DE LA RESTA ES: ", resta)
+print("EL RESULTADO DE LA DIVISION ES: ", divisioin)
+print("EL RESULTADO DE LA MULTIPLICACION ES: ", multiplicacion)
