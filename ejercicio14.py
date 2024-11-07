467a865259d1cc5e1be5162503236e165ba4d1f9
Santiago-Jared <santiagojaredavalos@gmail.com>
2024-11-07 13:44:27 -0600

    Create ejercicio14.py

---
+++ ejercicio14.py
@@ -0,0 +1,8 @@
+##14.-Dado un número de dos cifras, diseñe un algoritmo que permita obtener el número invertido
+
+cifra1 = f"Ingresa la primera cifra: "
+input(cifra1)
+cifra2 = f"Ingresa la segunda cifra: "
+input(cifra2)
+
+f"La cifra invertida de {cifra1} y {cifra2} es {cifra2} y {cifra1}"
