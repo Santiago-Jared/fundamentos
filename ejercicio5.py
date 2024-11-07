39e93aa10d4072c552b8b9c4403d55ac3f6fadb0
Santiago-Jared <santiagojaredavalos@gmail.com>
2024-11-07 13:39:27 -0600

    Create ejercicio5.py

---
+++ ejercicio5.py
@@ -0,0 +1,7 @@
+##5.- Escribir un programa que convierta un valor dado en grados Fahrenheit a grados celcius
+
+faren= input("Ingresa los grados Fahrenheit a convertir: ")
+faren= float(faren)
+celcius= (faren - 32) * 5/9
+
+print(faren, "Grados Fahrenheit es igual a la conversion de: ", celcius, "grados celcius")
