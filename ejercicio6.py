8d52203218e6a987720daeb123e7ad9027c53981
Santiago-Jared <santiagojaredavalos@gmail.com>
2024-11-07 13:40:01 -0600

    Create ejercicio6.py

---
+++ ejercicio6.py
@@ -0,0 +1,14 @@
+## 6.- Calcular la media de tres números pedidos por teclado
+
+
+num1= input("Ingresa el primer numero para determinar la MEDIANA: ")
+num1= int (num1)
+num2= input("Ingresa el segundo numero: ")
+num2= int (num2)
+num3= input("Ingresa el tercer nunero: ")
+num3= int (num3)
+
+prom = (num1+num2+num3) 
+mediana= prom/3
+
+print("La MEDIANA de los tres numeros es: ", mediana)
