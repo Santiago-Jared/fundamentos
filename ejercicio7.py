5d00ef8c089e6b99a4e548e371b36cbc6ca40a9a
Santiago-Jared <santiagojaredavalos@gmail.com>
2024-11-07 13:40:28 -0600

    Create ejercicio7.py

---
+++ ejercicio7.py
@@ -0,0 +1,7 @@
+## 7.- Realiza un programa que reciba una cantidad de minutos y muestre por pantalla a cuantas horas y minutos corresponde
+
+min= input("Ingresa los MOINUTOS a convertir an HORAS: ")
+min =int (min)
+
+hora= min/60
+print(min, "equivale a ", hora, "horas")
