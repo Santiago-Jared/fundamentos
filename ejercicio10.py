613f3e604bcff335d98fc9ab7176df4926b05bbc
Santiago-Jared <santiagojaredavalos@gmail.com>
2024-11-07 13:42:32 -0600

    Create ejercicio10.py

---
+++ ejercicio10.py
@@ -0,0 +1,9 @@
+##9._  Una tienda ofrece un desuento del 15% sobre el total de la compra y un cliente desea saber cuanto debera pagar por su compra.
+
+
+precio= f"Ingresa el precio del producto: "
+precio = input(precio)
+des = (precio * 0.15)
+destotal = (precio - des)
+
+f"Tu descuento por tu compra fue de {des} pesos, el descuento total es: {destotal}" 
