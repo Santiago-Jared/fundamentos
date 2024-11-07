d9961d2771aa51436240fb228aaf2dad68769ed1
Santiago-Jared <santiagojaredavalos@gmail.com>
2024-11-07 13:44:53 -0600

    Create ejercicio15.py

---
+++ ejercicio15.py
@@ -0,0 +1,9 @@
+##15.- Dadas dos variables numéricas A y B, que el usuario debe teclear, se pide
+##realizar un algoritmo que intercambie los valores de ambas variables y muestre
+## cuanto valen al final las dos variables
+
+A= f"Ingresa el  valor de A: "
+input(A)
+B= f"Ingresa el valor de B: "
+input(B)
+f"El valor de A es: {B} y el valor de B es: {A}"
